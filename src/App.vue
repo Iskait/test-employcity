14d8710d0d62ca07798a12fd5245d3ad8b8a98fe
Maxim Kuzin <iskait04@gmail.com>
2023-04-28 11:17:35 +0300

    Добавление компонентов на страницу

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from "@/components/the-header/TheHeader.vue";
+import TheHero from "@/components/TheHero.vue";
+import TheOrdering from "@/components/the-ordering/TheOrdering.vue";
+import TheFooter from "@/components/the-footer/TheFooter.vue";
+</script>
 
 <template>
-  <div></div>
+  <div class="wrapper">
+    <TheHeader />
+    <main class="main">
+      <TheHero />
+      <TheOrdering />
+    </main>
+    <TheFooter />
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
